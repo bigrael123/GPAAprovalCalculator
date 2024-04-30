ab5c874fed688bfd98b299ffc24498282dc49761
ThatGuy <41207027+bigrael123@users.noreply.github.com>
2024-04-30 10:20:01 +0000

    New Weight based score

--- .ccls-cache/@home@runner@ScoreCalculation/main.c
+++ .ccls-cache/@home@runner@ScoreCalculation/main.c
@@ -9,18 +9,13 @@ int GetNumGrades(){
   return NumberOfGrades;
 }
 
-//gets student's grade point average
-float getaverage( float TotalGrades, int NumberOfGrades){
-  float average = (TotalGrades)/NumberOfGrades;
-  return average;
-}
 //verifies if student was approved
 bool IsApproved(float average){
-  if(average<7){
+  if(average<70){
     return false;
   }
   else{
-    if(average >=7){
+    if(average >=70){
       return true;
     }
   }
@@ -31,19 +26,21 @@ int main(void) {
   int GottenNumberofGrades = GetNumGrades(); //Gets input from user about how many grades they have
   float SGrades[GottenNumberofGrades];// sets SGrades size array with the user input
   float Score; //This will hold the value of each grade into one number
+  float SWeight[GottenNumberofGrades]; //Will hold the value of the weight of each score
+  float TWeight = 0; //This will hold the value of each weight into one number
   for(int i = 0; i<GottenNumberofGrades; i++){ // for loop get input data from the user and combines them on the score
-    
     printf("Type your %d grade: ", i+1);
     scanf("%f", &SGrades[i]);
-    Score+=SGrades[i]; //gets grades and puts its value on one number
+    printf("What's the weight of grade %d: ", i+1);
+    scanf("%f", &SWeight[i]);
+    Score += (SWeight[i]/100)*SGrades[i]; //gets grades and puts its value on one number
   }
-  float Saverage = getaverage(Score, GottenNumberofGrades);//calculates the average grade by dividing the total Grades combined by the number of scores
-  if(IsApproved(Saverage)){ //gets the average and if approved displays text
-    printf("You have been approved with GPA of %g", Saverage);
+  if(IsApproved(Score)){ //gets the average and if approved displays text
+    printf("You have been approved with GPA of %g", Score);
   }
   else{
-    if(!IsApproved(Saverage))//gets the average and if reproved displays text
-     printf("You have been reproved with GPA of %g", Saverage);
+    if(!IsApproved(Score))//gets the average and if reproved displays text
+     printf("You have been reproved with GPA of %g", Score);
 
 
   }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 An evolved version of a college activity that calculates the GPA score, but you get to choose how many scores there are.
+You can get the weightless version on the WeightlessAverage branch.
 You can get the original version on the Legacy branch.
